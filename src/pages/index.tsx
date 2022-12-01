4d4e4583f240c5aed33f941d51cc1a2d483c1611
hiro <onecast.onelost615@gmail.com>
2022-12-01 12:34:39 +0700

    テスト記述・不要なのでその内削除します

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,19 @@
 import Head from "next/head";
+import { useCallback } from "react";
 import { Footer } from "src/components/Footer";
 import { Header } from "src/components/Header";
 import { Main } from "src/components/Main";
 import styles from "src/styles/Home.module.css";
 
 export default function Home() {
+  const foo = 1;
+
+  const handleClick = useCallback((e: { target: { href: any; }; preventDefault: () => void; }) => {
+    console.log(e.target.href);
+    e.preventDefault();
+    alert(foo);
+  }, []);
+
   return (
     <div className={styles.container}>
       <Head>
@@ -12,6 +21,10 @@ export default function Home() {
         <meta name="description" content="ここはindex pageです" />
       </Head>
       <Header />
+
+      <a href="/about" onClick={handleClick}>
+        ボタン
+      </a>
       <Main page="index" />
       <Footer />
     </div>
