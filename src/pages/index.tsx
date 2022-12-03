c6db05ac70c5c2e24b7b9df3551cfb3b0cdcb026
hiro <onecast.onelost615@gmail.com>
2022-12-03 12:33:52 +0700

    説明用のuseEffectを導入

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
 import Head from "next/head";
-import { useCallback } from "react";
+import { useCallback, useEffect } from "react";
 import { Footer } from "src/components/Footer";
 import { Header } from "src/components/Header";
 import { Main } from "src/components/Main";
@@ -8,10 +8,23 @@ import styles from "src/styles/Home.module.css";
 export default function Home() {
   const foo = 1;
 
-  const handleClick = useCallback((e: { target: { href: any; }; preventDefault: () => void; }) => {
-    console.log(e.target.href);
-    e.preventDefault();
-    alert(foo);
+  const handleClick = useCallback(
+    (e: { target: { href: any }; preventDefault: () => void }) => {
+      console.log(e.target.href);
+      e.preventDefault();
+      alert(foo);
+    },
+    []
+  );
+
+  useEffect(() => {
+    console.log("マウント時");
+    document.body.style.backgroundColor = "#c8c8c8";
+
+    return () => {
+      console.log("アンマウント時");
+      document.body.style.backgroundColor = "";
+    };
   }, []);
 
   return (
