669581030ea1f29a5dd77ec0fd9cbfd8bd624a2e
hiro <onecast.onelost615@gmail.com>
2022-12-16 18:39:03 +0900

    useStateでカウンター設置

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,22 +1,26 @@
 import Head from "next/head";
 import Link from "next/link";
-import { useCallback, useEffect } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { Footer } from "src/components/Footer";
 import { Header } from "src/components/Header";
 import { Main } from "src/components/Main";
 import styles from "src/styles/Home.module.css";
 
 export default function Home() {
-  const foo = 1;
+  const [foo, setFoo] = useState(1);
+  // let foo = 1;
+  const handleClick = (e: number) => {
+    setFoo((foo) => foo + 1);
+  };
 
-  const handleClick = useCallback(
-    (e: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => {
-      console.log(e.currentTarget.href);
-      e.preventDefault();
-      alert(foo);
-    },
-    []
-  );
+  // const handleClick = useCallback(
+  //   (e: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => {
+  //     console.log(e.currentTarget.href);
+  //     e.preventDefault();
+  //     alert(foo);
+  //   },
+  //   []
+  // );
 
   useEffect(() => {
     console.log("マウント時");
@@ -28,6 +32,8 @@ export default function Home() {
     };
   }, []);
 
+  // console.log(setFoo);
+
   return (
     <div className={styles.container}>
       <Head>
@@ -35,13 +41,13 @@ export default function Home() {
         <meta name="description" content="ここはindex pageです" />
       </Head>
       <Header />
-
-      <Link href="/about" onClick={handleClick}>
-        {/* <a href="/about" onClick={handleClick}> */}
-        ボタン
-        {/* </a> */}
-      </Link>
-
+      {/* <Link href="/about" onClick={handleClick}> */}
+      {/* <a href="/about" onClick={handleClick}> */}
+      {/* ボタン */}
+      {/* </a> */}
+      {/* </Link> */}
+      <h1>{foo}</h1>
+      <button onClick={handleClick}>ボタン</button>
       <Main page="index" />
       <Footer />
     </div>
