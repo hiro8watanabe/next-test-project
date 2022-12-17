1aed7b7c7aa3be2391eda8ddf56caa5f995fd4a7
hiro <onecast.onelost615@gmail.com>
2022-12-17 10:23:28 +0900

    useStateの文字列と真偽値のテストを追加

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
 import Head from "next/head";
 import Link from "next/link";
-import { useCallback, useEffect, useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import { Footer } from "src/components/Footer";
 import { Header } from "src/components/Header";
 import { Main } from "src/components/Main";
@@ -8,12 +8,36 @@ import styles from "src/styles/Home.module.css";
 
 export default function Home() {
   const [count, setCount] = useState(1);
+  const [text, setText] = useState("");
+  const [isShow, setIsShow] = useState(true);
+
   const handleClick = useCallback(() => {
     console.log(count);
     if (count < 10) {
       setCount((count) => count + 1);
     }
   }, [count]);
+
+  const handleChange = useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
+    if (e.target.value.length > 10) {
+      alert("10文字以内で入力してください");
+      return;
+    }
+    setText(e.target.value.trim());
+  }, []);
+
+  const handleFlag = useCallback(() => {
+    // setIsShow((isShow) => {
+    //   //▼最短の記述
+    //   return !isShow;
+
+    //   //▼以下と同じ意味になる
+    //   // return isShow ? false : true;
+    // });
+
+    //▼更にリファクタリング
+    setIsShow((isShow) => !isShow);
+  }, []);
 
   useEffect(() => {
     document.body.style.backgroundColor = "#c8c8c8";
@@ -35,8 +59,10 @@ export default function Home() {
       {/* ボタン */}
       {/* </a> */}
       {/* </Link> */}
-      <h1>{count}</h1>
+      {isShow ? <h1>{count}</h1> : null}
       <button onClick={handleClick}>ボタン</button>
+      <button onClick={handleFlag}>{isShow ? "非表示" : "表示"}</button>
+      <input type="text" value={text} onChange={handleChange} />
       <Main page="index" />
       <Footer />
     </div>
