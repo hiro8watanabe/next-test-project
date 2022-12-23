3d3f15c51ba5c4a1f76bd7e2bb42ccfe7b9bd313
hiro <onecast.onelost615@gmail.com>
2022-12-24 00:52:11 +0900

    コンポーネント間のstateリフトアップ

--- src/components/Headline/index.tsx
+++ src/components/Headline/index.tsx
@@ -4,18 +4,20 @@ import styles from "src/components/Headline/Headline.module.css";
 type Props = {
   page: string;
   children: ReactNode;
+  handleReduce: React.MouseEventHandler<HTMLButtonElement>;
 };
 
 export const Headline: React.FC<Props> = (props) => {
   const { page, children } = props;
   return (
-    <div>
+    <div className={styles.container}>
       <h1 className={styles.title}>
         Welcome to <a href="https://nextjs.org">Next.js! {page} Page</a>
       </h1>
 
-      <p className={styles.description}>Get started by editing {children}</p>
+      <p className={styles.description}>現在のページは{children}個です。</p>
       {/* <button onClick={props.onClick}>ボタン</button> */}
+      <button onClick={props.handleReduce}>減らす</button>
     </div>
   );
 };
--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,20 +1,59 @@
 import { Headline } from "src/components/Headline";
 import { Links } from "src/components/Links";
+import { useCallback, useState } from "react";
 import styles from "src/components/Main/Main.module.css";
 
 type Props = {
   page: string;
 };
 
+type Array = {
+  href: string;
+  title: string;
+  description: string;
+};
+
+const ITEMS: Array[] = [
+  {
+    href: "https://nextjs.org/docs",
+    title: "Documentation \u2192",
+    description: "Find in-depth information about Next.js features and API.",
+  },
+  {
+    href: "https://nextjs.org/learn",
+    title: "Learn →",
+    description: "Learn about Next.js in an interactive course with quizzes!",
+  },
+  {
+    href: "https://github.com/vercel/next.js/tree/canary/examples",
+    title: "Examples →",
+    description: "Discover and deploy boilerplate example Next.js projects.",
+  },
+  {
+    href: "https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app",
+    title: "Deploy →",
+    description:
+      "Instantly deploy your Next.js site to a public URL with Vercel.",
+  },
+];
+
 export const Main: React.FC<Props> = (props) => {
   const { page } = props;
+  const [items, setItems] = useState(ITEMS);
+  const handleReduce = useCallback(() => {
+    setItems((prevItems) => {
+      return prevItems.slice(0, prevItems.length - 1);
+    });
+  }, []);
+
   return (
     <main className={styles.main}>
-      <Headline page={page}>
+      <Headline page={page} handleReduce={handleReduce}>
         <code className={styles.code}>pages/{page}.tsx</code>
+        アイテム数は{items.length}
       </Headline>
 
-      <Links />
+      <Links items={items} handleReduce={handleReduce} />
     </main>
   );
 };
